104398377d789edaf6c9378f26ffefe743cf1169
Richard Steinbach <steinbach.dick@gmail.com>
2017-11-27 17:44:51 -0800

    add create_house.erb and form; add get action for create_house in house controller

--- app/controllers/house_controller.rb
+++ app/controllers/house_controller.rb
@@ -3,4 +3,10 @@ class HouseController < ApplicationController
   get '/houses/houses' do
     erb :'/houses/houses'
   end
+
+  get '/create_house' do
+    erb :'/houses/create_house'
+  end
+
+
 end
