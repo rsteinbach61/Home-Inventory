f452f8fd67fc81d2d098ca22417d5da8780ac69a
Richard Steinbach <steinbach.dick@gmail.com>
2017-11-27 17:59:15 -0800

    add post action with logged_in validation to house controller

--- app/controllers/house_controller.rb
+++ app/controllers/house_controller.rb
@@ -8,5 +8,15 @@ class HouseController < ApplicationController
     erb :'/houses/create_house'
   end
 
+  post '/new_house' do
+    if logged_in?
+      @house = House.create(params)
+      @house.user_id = current_user
+      @house.save
+    else
+      redirect to("/login")
+    end
+      redirect to("/houses/houses")
+  end
 
 end
