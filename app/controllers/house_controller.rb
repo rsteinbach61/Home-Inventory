05b8110f45cebef859f76613f1c7701a8979d10f
Richard Steinbach <steinbach.dick@gmail.com>
2017-11-28 13:49:43 -0800

    add links to houses/index and build houses/show page to display house name

--- app/controllers/house_controller.rb
+++ app/controllers/house_controller.rb
@@ -13,11 +13,19 @@ class HouseController < ApplicationController
       @house = House.create(params)
       @house.user_id = current_user.id
       @house.save
-      redirect to("/houses/show")
+      erb :"/houses/show"
     else
       redirect to("/login")
     end
+  end
 
+  get '/houses/:id' do
+    if logged_in?
+      @house = House.all.find(params[:id])
+      erb :'/houses/show'
+    else
+      redirect to('/login')
+    end
   end
 
 end
