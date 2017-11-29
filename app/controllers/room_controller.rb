27c15551de3405341b3552601f22ce99bf5b64e2
Richard Steinbach <steinbach.dick@gmail.com>
2017-11-29 11:50:13 -0800

    add get and post actions for rooms/new to room controller, and add new room form to rooms/new

--- app/controllers/room_controller.rb
+++ app/controllers/room_controller.rb
@@ -1,3 +1,21 @@
 class RoomController < ApplicationController
 
+  get '/rooms/new' do
+    if logged_in?
+      erb :'/rooms/new'
+    else
+      redirect to ('/login')
+    end
+
+    post '/rooms/new' do
+      if logged_in?
+        @user = current_user
+        @room = Room.create(params)
+        @room.user_id = @user.id
+        @room.save
+        erb :'/rooms/show'
+      else
+        redirect to ('/login')
+      end
+    end
 end
